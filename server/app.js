3b5ea02e58191c9c2191374be82f22f849d4252b
Aaron Sawyer <me@aarondevon.com>
2015-05-26 14:53:05 -0500

    Server app.js working

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var app =express();
+var app = express();
 
 app.get("/", function(request, respond){
-    respond.send("I am here");
+    respond.send("I am here... I think.");
 });
 
 var server = app.listen(3000, function(){
    var port = server.address().port;
-    console.log("Listening on port: ", port);
+    console.log("I hear things on port: ", port);
 });
