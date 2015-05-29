0bcb1fe620a8209ad61b9f64f45c9cd988682317
Aaron Sawyer <me@aarondevon.com>
2015-05-29 10:26:29 -0500

    Adds DB functionality... but doesn't work

--- client/app.js
+++ client/app.js
@@ -1,12 +1,47 @@
 var carApp = angular.module('carApp',[]);
 
 carApp.controller('automobile',["$scope", "$http", function($scope, $http){
-    $scope.heading = "Message: ";
-    $scope.persons = [];
 
-    $scope.updateMessage = function(){
-        $scope.message = "Hello, " + $scope.name + "!";
+    $scope.mileage = {};
+    $scope.gallons = {};
+    $scope.mpgArray = [];
+
+    var fetchMPG = function() {
+        return $http.get('/mpgdata').then(function(response){
+            if(response.status !== 200){
+                throw new Error('Failed to fetch cats from the API');
+            }
+            $scope.mileage = {};
+            $scope.gallons = {};
+            $scope.mpgArray = response.data;
+            return response.data;
+        })
     };
 
+    //$scope.mpg = function() {
+    //    $scope.calculateMpg = $scope.mileage / $scope.gallons;
+    //    console.log($scope.calculateMpg);
 
+        $scope.mpg = function(mileage, gallons) {
+            var average = mileage / gallons;
+            console.log(average);
+            return $http.post('/add', average).then(fetchMPG);
+
+        };
+    //};
 }]);
+
+
+
+
+
+
+
+
+
+
+
+$scope.add = function(cat){
+    return $http.post('/add', cat).then(fetchCats);
+};
+fetchCats();
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,24 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/auto_stats');
 
+var MileItUp = mongoose.model('MileItUp', {test: Number});
+
+router.post('/add', function(request, response, next){
+    var kitty = new MileItUp({test: request.body.mileage});
+    kitty.save(function(err){
+        if(err) console.log('meow %s', err);
+        response.send(kitty.toJSON());
+        //next();
+    });
+});
+
+router.get('/mpgdata', function(request, response, next){
+    return Cat.find({}).exec(function(err, cats){
+        if(err) throw new Error(err);
+        response.send(JSON.stringify(cats));
+        //next();
+    });
+});
 
 router.get("/*", function(request, respond, next){
     var file = request.params[0] || 'views/index.html';
